e76c423cb43ce1b9bedad2b14ee6dc18f7ea73c3
Yixuan Qiu <yixuan.qiu@cos.name>
2015-09-22 20:48:43 -0400

    overloads

--- java/statr/rmath/Rmath.java
+++ java/statr/rmath/Rmath.java
@@ -34,16 +34,25 @@ public class Rmath {
     public static double dnorm(double x, double mu, double sigma, boolean lg) {
         return Instance.dnorm4(x, mu, sigma, lg ? 1 : 0);
     }
+    public static double dnorm(double x, double mu, double sigma) {
+        return Instance.dnorm4(x, mu, sigma, 0);
+    }
     public static double pnorm(double x, double mu, double sigma, boolean lt, boolean lg) {
         return Instance.pnorm5(x, mu, sigma, lt ? 1 : 0, lg ? 1 : 0);
     }
+    public static double pnorm(double x, double mu, double sigma) {
+        return Instance.pnorm5(x, mu, sigma, 1, 0);
+    }
     public static double qnorm(double p, double mu, double sigma, boolean lt, boolean lg) {
         return Instance.qnorm5(p, mu, sigma, lt ? 1 : 0, lg ? 1 : 0);
+    }
+    public static double qnorm(double p, double mu, double sigma) {
+        return Instance.qnorm5(p, mu, sigma, 1, 0);
     }
 
 
 
     public static void main(String[] args) {
-        System.out.println("dnorm(1.2, mu = 3.4, sd = 5.6) = " + Rmath.dnorm(1.2, 3.4, 5.6, false));
+        System.out.println("dnorm(1.2, mu = 3.4, sd = 5.6) = " + Rmath.dnorm(1.2, 3.4, 5.6));
     }
 }
